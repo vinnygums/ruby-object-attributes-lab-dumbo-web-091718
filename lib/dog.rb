067ed261488bd94c3ac40174501caecafb2955a4
vinny_gums <vicente.soriano@icloud.com>
2018-09-25 15:44:31 -0400

    Done

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+
+  def name
+      @name
+  end
+
+  def name=(name_input)
+    @name = name_input
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed_input)
+    @breed = breed_input
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job_input)
+    @job = job_input
+  end
+
+end
